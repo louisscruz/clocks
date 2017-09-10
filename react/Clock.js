bb2edf5e4ee9e18aaae9e2269d1e9bb41bc7828b
louisscruz <louisstephancruz@me.com>
2017-09-10 16:45:23 -0700

    Add hands

--- react/Clock.js
+++ react/Clock.js
@@ -47,7 +47,68 @@ class Clock extends React.Component {
     );
   }
 
-  render() {
+  getVal(type) {
+    const { currentTime } = this.state;
+
+    if (type === 'hours') {
+      return currentTime.getHours();
+    } else if (type === 'minutes') {
+      return currentTime.getMinutes()
+    } else if (type === 'seconds') {
+      return currentTime.getSeconds();
+    }
+  }
+
+  getDegree(hand, val) {
+    if (hand === 'hours') {
+      return this.hoursDegree(val);
+    } else if (hand === 'minutes' || hand === 'seconds') {
+      return this.degree(val);
+    }
+  }
+
+  degree(val) {
+    return val * 6;
+  }
+
+  hoursDegree(val) {
+    return val * 15;
+  }
+
+  handStyles(hand) {
+    const val = this.getVal(hand);
+    const degree = this.getDegree(hand, val);
+    return {
+      transform: `rotate(${degree}deg)`
+    };
+  }
+
+  generateHands() {
+    const hoursHand = React.createElement(
+      'div',
+      {className: 'clock-hand hours', key: 123, style: this.handStyles('hours')}
+    );
+    const minutesHand = React.createElement(
+      'div',
+      {className: 'clock-hand hours', key: 234, style: this.handStyles('minutes')}
+    );
+    const secondsHand = React.createElement(
+      'div',
+      {className: 'clock-hand seconds', key: 345, style: this.handStyles('seconds')}
+    );
+
+    return React.createElement(
+      'div',
+      {className: 'clock-hand-container', key: 89},
+      [
+        hoursHand,
+        minutesHand,
+        secondsHand
+      ]
+    );
+  }
+
+  generateButtons() {
     const startButton = React.createElement(
       ClockButton,
       {start: () => this.start(), key: 1}
@@ -56,12 +117,17 @@ class Clock extends React.Component {
       ClockButton,
       {stop: () => this.stop(), key: 2}
     );
-    const buttons = React.createElement(
+    return React.createElement(
       'div',
       {key: 4},
       [startButton, stopButton]
     );
+  }
 
-    return React.createElement('div', null, [this.formattedTime(), buttons]);
+  render() {
+    const hands = this.generateHands();
+    const buttons = this.generateButtons();
+
+    return React.createElement('div', null, [this.formattedTime(), hands, buttons]);
   }
 }
